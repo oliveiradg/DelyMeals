6b36b763019b23109e5bb3dc54cfce22a91a0343
João Oliveira <j.oliveiradg@gmail.com>
2023-02-07 22:31:20 -0300

    [DIA 30/100] feat: Starting With The App - Adding Base Data

---
+++ lib/views/categories_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CategoriesView extends StatelessWidget {
+  const CategoriesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: [],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
